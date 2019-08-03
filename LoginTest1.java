299ab13f1dac37b8f8a90c461827753a7f9c385b
Piyush Warke <44377484+piyush19198@users.noreply.github.com>
2019-08-03 22:25:19 +0530

    Create LoginTest1.java

---
+++ LoginTest1.java
@@ -0,0 +1,50 @@
+package comm.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextField;
+
+public class Login_AWT{
+	public static void main(String[] args){
+		Frame frame = new Frame("AWT Login");
+		frame.setVisible(true);
+		
+		Label lblLoginName, lblPassword;
+		TextField txtLoginName, txtPassword;
+		Button btnOk, btnCancel;
+		
+		Panel panelMain, panelLogin, panelButton;
+		
+		lblLoginName = new Label("Login Name");
+		lblPassword = new Label("Password");
+		
+		txtLoginName = new TextField(10);
+		txtPassword = new TextField(10);
+		
+		btnOk = new Button("OK");
+		btnCancel = new Button("Cancel");
+		
+		panelMain = new Panel();
+		panelLogin = new Panel();
+		panelButton = new Panel();
+		
+		panelLogin.add(lblLoginName);
+		panelLogin.add(txtLoginName);
+		panelLogin.add(lblPassword);
+		panelLogin.add(txtPassword);
+		
+		panelButton.add(btnOk);
+		panelButton.add(btnCancel);
+		
+		panelMain.setLayout(new BorderLayout());
+		panelMain.add(panelLogin, BorderLayout.CENTER);
+		panelMain.add(panelButton, BorderLayout.SOUTH);
+		
+		frame.add(panelMain);
+		frame.pack();
+		frame.setResizable(false);
+	}
+}
